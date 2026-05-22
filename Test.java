c7fe495fe12401cb1399c72f446a6f51f0a69b77
AnantBhardwaj2004 <bhardwajanant93@gmail.com>
2026-05-22 11:50:40 +0530

    updated

--- Test.java
+++ Test.java
@@ -2,10 +2,10 @@ public class Test {
 
   static void add(){
 
-    int a=48;
-    int b=65;
-    int result = a+b;
-    System.out.println("The sum is " +result);
+    int a=10;
+    int b=60;
+    int result = a*b;
+    System.out.println("The product is " +result);
     
   }
   public static void main(String[] args)
